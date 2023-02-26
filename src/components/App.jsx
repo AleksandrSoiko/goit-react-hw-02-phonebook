d279ad16e5b9236761ade89776f6ddf01d1286ee
AleksandrSoiko <farshchef777@gmail.com>
2023-02-26 11:27:15 +0000

    result

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,51 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework template2
-    </div>
-  );
-};
+import { Component } from 'react';
+import { ListContact } from './ListContacts/ListContacts';
+import { Layout } from './Layout/Layout.styled';
+import { ContactForm } from './FormsAdd/FormsAdd';
+import { nanoid } from 'nanoid';
+import { Title } from './App.styled';
+import { Filter } from './FilterContacts/FilterContacts';
+import phones from '../../src/data.json';
+
+export class App extends Component {
+  state = {
+    contacts: [...phones],
+    filter: '',
+  };
+
+  onDelete = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  onSubmit = data => {
+    const equalName = this.state.contacts.find(
+      el => el.name.toLowerCase() === data.name.toLowerCase()
+    );
+    if (equalName) return alert(equalName.name + ' is already in contacts.');
+
+    data.id = nanoid();
+    this.setState(prev => ({ contacts: [data, ...prev.contacts] }));
+  };
+
+  filterName = event => {
+    this.setState({ filter: event.currentTarget.value });
+  };
+
+  render() {
+    const filterNormilized = this.state.filter.toLowerCase().trim();
+    const visibleContacts = this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filterNormilized)
+    );
+    return (
+      <Layout>
+        <Title>Phonebook</Title>
+        <ContactForm onSubmit={this.onSubmit}></ContactForm>
+        <Title>Contacts</Title>
+        <Filter value={this.state.filter} OnFilterChange={this.filterName} />
+        <ListContact contacts={visibleContacts} onDelete={this.onDelete} />
+      </Layout>
+    );
+  }
+}
